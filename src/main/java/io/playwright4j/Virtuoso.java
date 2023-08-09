e9ae4bea32a92b9a177b781021da45a3a98f2cc9
SergeyPirogov <serhii.pirogov@gmail.com>
2023-08-09 18:33:07 +0300

    add $ for role

--- src/main/java/io/playwright4j/Virtuoso.java
+++ src/main/java/io/playwright4j/Virtuoso.java
@@ -2,6 +2,7 @@ package io.playwright4j;
 
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
 
 import static io.playwright4j.core.PWWrapper.pwContainer;
 
@@ -13,6 +14,10 @@ public class Virtuoso {
 
     public static Locator $(String locator) {
         return page().locator(locator);
+    }
+
+    public static Locator $(AriaRole role, String text) {
+        return page().getByRole(role, new Page.GetByRoleOptions().setName(text));
     }
 
     public static void open(String url) {
